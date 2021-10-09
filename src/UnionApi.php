fcfa48e93a45f836d89b578dc103b008ba892ac0
peter <perter@126.com>
2021-10-09 16:51:36 +0800

    init

--- src/UnionApi.php
+++ src/UnionApi.php
@@ -6,35 +6,193 @@ use by\infrastructure\helper\CallResultHelper;
 
 class UnionApi extends BaseApi
 {
+    static $dataCodeMethods = [
+        'getmarrycode',
+        'getpolitcode',
+        'geteducode',
+        //学历）
+        'getschoolcode',
+        //院校）
+        'getspeccode',
+        //专业）
+        'getjobcode',
+        //岗位类型）
+        'getprofcode',
+        //职称）
+        'getworktypecode',
+        //工作类型）
+        'getworkstatuscode',
+        //在职情况）
+        'getarrivecode', //到岗时间）
+        'getareacode', //地区）
+        'getlangcode', //语言）
+        'getlevelcode', //语言水平）
+        'getenglishcode', //英语水平）
+        'getspeakcode', //口语水平）
+        'getindustrycode', //行业）
+        'getcompanytypecode', //公司类型）
+        'getwelfarecode',//福利）
+    ];
 
-    public function createResume(ResumePo $po) {
+    public function getareacode()
+    {
+        return $this->wrapResult('/getareacode');
+    }
+    public function getlangcode()
+    {
+        return $this->wrapResult('/getlangcode');
+    }
+
+    public function getlevelcode()
+    {
+        return $this->wrapResult('/getlevelcode');
+    }
+
+    public function getenglishcode()
+    {
+        return $this->wrapResult('/getenglishcode');
+    }
+    public function getspeakcode()
+    {
+        return $this->wrapResult('/getspeakcode');
+    }
+    public function getindustrycode()
+    {
+        return $this->wrapResult('/getindustrycode');
+    }
+    public function getcompanytypecode()
+    {
+        return $this->wrapResult('/getcompanytypecode');
+    }
+    public function getwelfarecode()
+    {
+        return $this->wrapResult('/getwelfarecode');
+    }
+
+    public function getarrivecode()
+    {
+        return $this->wrapResult('/getarrivecode');
+    }
+
+    public function getworkstatuscode()
+    {
+        return $this->wrapResult('/getworkstatuscode');
+    }
+
+    public function getworktypecode()
+    {
+        return $this->wrapResult('/getworktypecode');
+    }
+
+    public function getprofcode()
+    {
+        return $this->wrapResult('/getprofcode');
+    }
+    public function getjobcode()
+    {
+        return $this->wrapResult('/getjobcode');
+    }
+
+    public function getspeccode()
+    {
+        return $this->wrapResult('/getspeccode');
+    }
+
+    public function getschoolcode()
+    {
+        return $this->wrapResult('/getschoolcode');
+    }
+
+    public function geteducode()
+    {
+        return $this->wrapResult('/geteducode');
+    }
+
+    public function getpolitcode()
+    {
+        return $this->wrapResult('/getpolitcode');
+    }
+
+    public function getmarrycode()
+    {
+        return $this->wrapResult('/getmarrycode');
+    }
+
+    public function applyresume()
+    {
+        return $this->wrapResult('/applyresume', [], 'resume');
+    }
+    // （应聘信息）
+    // 需userid和token参数，根据身份校验判断是企业还是个
+    public function applydata($data = [], $page = 1, $size = 20)
+    {
+//        from	来源，值为net(网络),market(现场招聘会)
+//key	关键词搜索
+//type	数据类型，值为1(网络应聘),2(网络面试)
+//marketid	现场招聘会编号
+//orderid	现场招聘会预定编号
+        $data['page'] = $page;
+        $data['size'] = $size;
+        return $this->wrapResult('/applydata', $data, 'resume');
+    }
+
+    public function queryResume($data = [
+        'page' => 1,
+        'size' => 20
+    ])
+    {
+//        参数	说明
+//resumeid	简历编号
+//jobid	岗位编号，判断简历是否应聘此岗位
+//area	意向地区搜索，值来自地区数据接口
+//spec	专业关键词搜索
+//edu	学历搜索，值来自学历数据接口
+//worktime	工作经验搜索
+//minsalary	最低薪资搜索
+//maxsalary	最高薪资搜索
+//key	关键字搜索
+//size	单页数量，默认20
+//page	第几页，默认1
+        return $this->wrapResult('/getresume', $data, 'resume');
+    }
+
+    public function resumerefresh($id)
+    {
+        return $this->wrapResult('/resumerefresh', ['id' => $id]);
+    }
+
+    public function createResume(ResumePo $po)
+    {
         return $this->editResume(0, $po);
     }
 
-    public function editResume($id, ResumePo $po) {
+    public function editResume($id, ResumePo $po)
+    {
         return $this->wrapResult('/editresume', array_merge(['id' => $id], $po->toArray()));
     }
 
     public function applyjob($data = [
         'from' => 'net',
-//        from	来源，值为net(网络),market(现场招聘会)
-//resumeid	简历编号
-//jobid	岗位编号
-//marketid	招聘会编号
-//content	应聘岗位留言，可为空
-    ]) {
+        //        from	来源，值为net(网络),market(现场招聘会)
+        //resumeid	简历编号
+        //jobid	岗位编号
+        //marketid	招聘会编号
+        //content	应聘岗位留言，可为空
+    ])
+    {
         return $this->wrapResult('/applyjob', $data);
     }
 
     public function getnetmarketjobs($data = [
-        'page' => 1,
-        'size' => 20,
-        'marketid' => 0,
-//        'compid' => '',
-//        'jobid' => '',
-        'key' => '',
+        'page'      => 1,
+        'size'      => 20,
+        'marketid'  => 0,
+        //        'compid' => '',
+        //        'jobid' => '',
+        'key'       => '',
         'supersort' => '',
-    ]) {
+    ])
+    {
         return $this->wrapResult('/getnetmarketjobs', $data, 'jobs');
     }
 
@@ -43,11 +201,17 @@ class UnionApi extends BaseApi
      * @param $id
      * @return \by\infrastructure\base\CallResult
      */
-    public function getJobInfo($id) {
+    public function getJobInfo($id)
+    {
         return $this->wrapResult('/getjobinfo', ['id' => $id], 'job');
     }
 
-    public function loginByMobile($data = ['mobile' => '', 'code' => '', 'day' => 1]) {
+    public function loginByMobile($data = [
+        'mobile' => '',
+        'code'   => '',
+        'day'    => 1
+    ])
+    {
         return $this->wrapResult('/mobileandcode', $data, 'account');
     }
 
@@ -119,7 +283,14 @@ class UnionApi extends BaseApi
                 return CallResultHelper::fail($service . '返回成功但没有' . $checkSucKey . '参数');
             }
         }
-        return CallResultHelper::fail($json['info']);
+        if (array_key_exists('info', $json)) {
+            return CallResultHelper::fail($json['info']);
+        } else {
+            if (in_array(ltrim($service, '/'), self::$dataCodeMethods)) {
+                return CallResultHelper::success($json);
+            }
+            return CallResultHelper::fail($json);
+        }
     }
 
 
--- tests/demo.php
+++ tests/demo.php
@@ -12,6 +12,11 @@ $code = '092110';
 $uid = '2879404';
 $token = '1EEB3F4F7B14D905ACC60F6A8D44F734#1633766401#1';
 $token2= '28BD2FE52E12034AAFED19616E758477#1633766501#1';
+//$api->setUserid($uid);
+//$api->setToken($token);
+var_dump($api->getschoolcode());
+//var_dump($api->applydata(['from' => 'net']));
+//var_dump($api->queryResume());
 //var_dump($api->logout());
 
 //var_dump($api->sendcode($mobile));
@@ -21,7 +26,5 @@ $token2= '28BD2FE52E12034AAFED19616E758477#1633766501#1';
 
 //var_dump($api->isUserExist('18557515452'));
 
-//$api->setUserid($uid);
-//$api->setToken($token);
-var_dump($api->getnetmarketjobs(['jobid' => 10130, 'marketid' => 7, 'page' => 1, 'size' => 20]));
+//var_dump($api->getnetmarketjobs(['jobid' => 10130, 'marketid' => 7, 'page' => 1, 'size' => 20]));
 //var_dump($api->getJobInfo(10130));
